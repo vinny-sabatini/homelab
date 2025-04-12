c17e515b9e7e6f23ac00d0dcc94e773b4c8cd37b
Vinny Sabatini <vincent.sabatini@gmail.com>
2025-04-11 21:44:03 -0500

    manage manually installed applications using Argo
    - ArgoCD installation
    - Cilium installation (with gateway api) and configuration

--- scripts/03-install-argocd.sh
+++ scripts/03-install-argocd.sh
@@ -4,5 +4,5 @@ set -euo
 
 kubectl create namespace argocd
 helm repo add argo https://argoproj.github.io/argo-helm
-helm install cluster argo/argo-cd --namespace argocd
+helm install cluster argo/argo-cd --namespace argocd --set fullnameOverride=argocd
 kubectl apply -f cluster-app.yaml
